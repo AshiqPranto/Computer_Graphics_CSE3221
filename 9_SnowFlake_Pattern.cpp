4da058cabda67c20dd8729d7fbd8fb97e09a213a
AshiqPranto <ashiqpranto@gmail.com>
2023-07-10 08:30:37 +0600

    updated SnowFlake Pattern

--- 9_SnowFlake_Pattern.cpp
+++ 9_SnowFlake_Pattern.cpp
@@ -1,47 +1,43 @@
-#include <iostream>
-#include <math.h>
 #include <graphics.h>
-using namespace std;
-class kochCurve
-{
-    public:
-    void koch(int it,int x1,int y1,int x5,int y5)
-    {
-        int x2,y2,x3,y3,x4,y4;
-        int dx,dy;
-        if (it==0)
-        {
-            line(x1,y1,x5,y5);
-        }
-        else
-        {
-            delay(100);
-            dx=(x5-x1)/3;
-            dy=(y5-y1)/3;
-            x2=x1+dx;
-            y2=y1+dy;
-            x3=(int)(0.5*(x1+x5)+sqrt(3)*(y1-y5)/6);
-            y3=(int)(0.5*(y1+y5)+sqrt(3)*(x5-x1)/6);
-            x4=2*dx+x1;
-            y4=2*dy+y1;
-            koch(it-1,x1,y1,x2,y2);
-            koch(it-1,x2,y2,x3,y3);
-            koch(it-1,x3,y3,x4,y4);
-            koch(it-1,x4,y4,x5,y5);
-        }
+#include <math.h>
+
+void drawKochCurve(int x1, int y1, int x2, int y2, int iteration) {
+    if (iteration == 0) {
+        line(x1, y1, x2, y2);
     }
-};
-int main()
-{
-    kochCurve k;
-    int it;
-    cout<<"Enter Number Of Iterations : "<<endl;
-    cin>>it;
-    int gd=DETECT,gm;
-    initgraph(&gd,&gm,NULL);
-    k.koch(it,150,20,20,280);
-    k.koch(it,280,280,150,20);
-    k.koch(it,20,280,280,280);
+    else {
+        int deltaX = x2 - x1;
+        int deltaY = y2 - y1;
+
+        int xA = x1 + deltaX / 3;
+        int yA = y1 + deltaY / 3;
+
+        int xB = x1 + 2 * deltaX / 3;
+        int yB = y1 + 2 * deltaY / 3;
+
+        int xC = xA + (int)(cos(M_PI / 3) * (xB - xA)) - (int)(sin(M_PI / 3) * (yB - yA));
+        int yC = yA + (int)(sin(M_PI / 3) * (xB - xA)) + (int)(cos(M_PI / 3) * (yB - yA));
+
+        drawKochCurve(x1, y1, xA, yA, iteration - 1);
+        drawKochCurve(xA, yA, xC, yC, iteration - 1);
+        drawKochCurve(xC, yC, xB, yB, iteration - 1);
+        drawKochCurve(xB, yB, x2, y2, iteration - 1);
+    }
+}
+
+int main() {
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "");
+
+    //int x1 = 100, y1 = 300;
+    //int x2 = 500, y2 = 300;
+
+    int iteration = 3;
+
+    drawKochCurve(20, 280, 280, 280, iteration);
+    drawKochCurve(280, 280, 150, 20, iteration);
+    drawKochCurve(150, 20, 20,280, iteration);
+
     getch();
     closegraph();
     return 0;
